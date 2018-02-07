446396aa4e2b326ece3fa4924ff0cabbf84579af
Hugo Alliaume <kocal@live.fr>
2018-02-07 21:35:51 +0100

    Tests: display file content if test is failing

--- tests/test_axios.sh
+++ tests/test_axios.sh
@@ -9,5 +9,6 @@ echo "Checking Axios..."
 
 if ! grep -q "axios" package.json; then
     echo "Axios not found in package.json"
+    cat package.json
     exit 1
 fi;
--- tests/test_router.sh
+++ tests/test_router.sh
@@ -9,6 +9,7 @@ echo "Checking Vue-Router..."
 
 if ! grep -q "vue-router" package.json; then
     echo "Vue-Router not found in package.json"
+    cat packages.json
     exit 1
 fi;
 
@@ -18,11 +19,13 @@ test -f src/popup/router/routes.js
 
 if ! grep -ql "^import store from './router';$" src/popup/popup.js; then
     echo "Line « import store from './router'; » not found in src/popup/popup.js"
+    cat src/popup/popup.js
     exit 2
 fi;
 
 if ! grep -ql "^[[:space:]][[:space:]]router,$" src/popup/popup.js; then
     echo "Line « router, » not found in src/popup/popup.js"
+    cat src/popup/popup.js
     exit 2
 fi;
 
