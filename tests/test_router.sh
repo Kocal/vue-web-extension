be551e5b617f91f71d901d8f84f844b95ceb38a4
Hugo Alliaume <kocal@live.fr>
2018-02-07 21:29:13 +0100

    Implement Vue-Router tests

--- tests/test_router.sh
+++ tests/test_router.sh
@@ -1,9 +1,28 @@
 #!/usr/bin/env bash
 
-set -e
+set -ev
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-cd "$DIR"
+cd "$DIR/output"
 
-echo "$DIR"
 echo "Checking Vue-Router..."
+
+if ! grep -q "vue-router" package.json; then
+    echo "Vue-Router not found in package.json"
+    exit 1
+fi;
+
+test -f src/popup/router/pages/Index.vue
+test -f src/popup/router/index.js
+test -f src/popup/router/routes.js
+
+if ! grep -ql "^import store from './../store';$" src/popup/popup.js; then
+    echo "Line « import store from './../store'; » not found in src/popup/popup.js"
+    exit 2
+fi;
+
+if ! grep -ql "^[[:space:]][[:space:]]router,$" src/popup/popup.js; then
+    echo "Line « router, » not found in src/popup/popup.js"
+    exit 2
+fi;
+
