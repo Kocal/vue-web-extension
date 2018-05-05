cb5e62cf0016c138a91475a13105004a72b12f07
Hugo Alliaume <kocal@live.fr>
2018-05-05 19:08:07 +0200

    refactor: improve template testing (#62)
    * test: add scenarios
    
    * ci: remove apt steps
    
    * feat(template): use scenarios
    
    * refactor(ci): rewrite matrix, remove generate_extension script
    
    * chore: fix router/store tests due to last style changes

--- tests/test_router.sh
+++ tests/test_router.sh
@@ -2,9 +2,6 @@
 
 set -ev
 
-DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-cd "$DIR/output"
-
 if ! grep -q "vue-router" package.json; then
     echo "Vue-Router not found in package.json"
     cat packages.json
@@ -15,8 +12,8 @@ test -f src/popup/router/pages/Index.vue
 test -f src/popup/router/index.js
 test -f src/popup/router/routes.js
 
-if ! grep -ql "^import router from './router';$" src/popup/popup.js; then
-    echo "Line « import router from './router'; » not found in src/popup/popup.js"
+if ! grep -ql "^import router from './router'$" src/popup/popup.js; then
+    echo "Line « import router from './router' » not found in src/popup/popup.js"
     cat src/popup/popup.js
     exit 2
 fi;
--- tests/test_store.sh
+++ tests/test_store.sh
@@ -2,9 +2,6 @@
 
 set -ev
 
-DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-cd "$DIR/output"
-
 if ! grep -q "vuex" package.json; then
     echo "Vuex not found in package.json"
     cat packages.json
@@ -17,14 +14,14 @@ test -f src/store/index.js
 test -f src/store/mutation-types.js
 test -f src/store/mutations.js
 
-if ! grep -ql "^import store from './store';$" src/background.js; then
-    echo "Line « import store from './store'; » not found in src/background.js"
+if ! grep -ql "^import store from './store'$" src/background.js; then
+    echo "Line « import store from './store' » not found in src/background.js"
     cat src/background.js
     exit 2
 fi;
 
-if ! grep -ql "^import store from './../store';$" src/popup/popup.js; then
-    echo "Line « import store from './../store'; » not found in src/popup/popup.js"
+if ! grep -ql "^import store from '../store'$" src/popup/popup.js; then
+    echo "Line « import store from '../store' » not found in src/popup/popup.js"
     cat src/popup/popup.js
     exit 2
 fi;
