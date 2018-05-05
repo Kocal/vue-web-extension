892713c723e7d11b7d55de397cbb98913b48b8e3
Hugo Alliaume <kocal@live.fr>
2018-05-05 15:24:00 +0200

    feat: add Hot Module Reloading (#61)
    * feat: enable hot module reloading
    
    * docs(README): add HMR
    
    * chore: forgot HMR flag for watch:dev script

--- template/webpack.config.js
+++ template/webpack.config.js
@@ -2,6 +2,7 @@ const webpack = require('webpack');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const WebpackShellPlugin = require('webpack-shell-plugin');
 const CopyWebpackPlugin = require('copy-webpack-plugin');
+const ChromeExtensionReloader = require('webpack-chrome-extension-reloader');
 const { VueLoaderPlugin } = require('vue-loader');
 const { version } = require('./package.json');
 
@@ -87,6 +88,12 @@ if (config.mode === 'production') {
         NODE_ENV: '"production"',
       },
     }),
+  ]);
+}
+
+if (process.env.HMR === 'true') {
+  config.plugins = (config.plugins || []).concat([
+    new ChromeExtensionReloader(),
   ]);
 }
 
