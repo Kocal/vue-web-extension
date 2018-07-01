a94956dc10a4a79b5a9af8f18496d4b85916b734
Hugo Alliaume <kocal@live.fr>
2018-07-01 21:24:15 +0200

    feat(eslint): add web extension environment

--- examples/full/.eslintrc.js
+++ examples/full/.eslintrc.js
@@ -8,6 +8,7 @@ module.exports = {
   },
   env: {
     browser: true,
+    webextensions: true,
   },
   extends: [
     // https://github.com/vuejs/eslint-plugin-vue#priority-a-essential-error-prevention
