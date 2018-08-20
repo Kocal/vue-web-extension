61ba8672413410cff19d8a744c0bbf4482ddab80
Hugo Alliaume <kocal@live.fr>
2018-08-20 19:30:03 +0200

    chore(ci): tweaks dependabot detection + improve readability + improve workflow
    oops :s

--- tests/test_router.sh
+++ tests/test_router.sh
@@ -12,13 +12,13 @@ test -f src/popup/router/pages/Index.vue
 test -f src/popup/router/index.js
 test -f src/popup/router/routes.js
 
-if ! grep -ql "^import router from './router'$" src/popup/popup.js; then
+if ! grep -ql "import router from './router'" src/popup/popup.js; then
     echo "Line « import router from './router' » not found in src/popup/popup.js"
     cat src/popup/popup.js
     exit 2
 fi;
 
-if ! grep -ql "^[[:space:]][[:space:]]router,$" src/popup/popup.js; then
+if ! grep -ql "[[:space:]][[:space:]]router," src/popup/popup.js; then
     echo "Line « router, » not found in src/popup/popup.js"
     cat src/popup/popup.js
     exit 2
