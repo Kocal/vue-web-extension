66464ba0edbc0556d8db297a55e523fe2e41ffd6
Hugo Alliaume <kocal@live.fr>
2018-08-27 09:11:44 +0200

    implement version testing for appveyor

---
+++ tests/test_version.ps1
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+
+Write-Host "Checking version behavior..."
+(Get-Content './src/manifest.json' | ConvertFrom-Json).version| Assert-StringEqual -Expected  "null"
+(Get-Content './package.json' | ConvertFrom-Json).version | Assert-StringEqual -Expected "1.0.0"
+(Get-Content './dist/manifest.json' | ConvertFrom-Json).version | Assert-StringEqual -Expected "1.0.0"
+
+Write-Host "Upgrade version then build..."
+yarn version --new-version major --no-git-tag-version
+yarn build --display errors-only
+
+Write-Host "Version should be updated"
+(Get-Content './src/manifest.json' | ConvertFrom-Json).version| Assert-StringEqual -Expected  "null"
+(Get-Content './package.json' | ConvertFrom-Json).version | Assert-StringEqual -Expected "2.0.0"
+(Get-Content './dist/manifest.json' | ConvertFrom-Json).version | Assert-StringEqual -Expected "2.0.0"
